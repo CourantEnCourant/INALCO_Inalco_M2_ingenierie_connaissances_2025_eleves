d4c91b240b430eb9aeab02d697ec70021869bdad
Weiqi ZHANG <weiqi.zhang_arthur@yahoo.com>
2025-03-29 00:17:55 +0100

    almost finished tf-idf part

--- TP/utils.py
+++ TP/utils.py
@@ -3,6 +3,8 @@ import spacy
 from spacy.tokens.token import Token
 from spacy.tokens.doc import Doc
 
+from sklearn.feature_extraction.text import TfidfVectorizer
+import numpy as np
 
 from tqdm import tqdm
 from pathlib import Path
@@ -28,14 +30,15 @@ def texts_from_folder(folder: Path = Path("lotr_corpus")) -> list[str]:
     return texts
 
 
+# A magical higher-order function
 def build_pipe(*funcs):
     """Combine functions into one"""
     return lambda x: reduce(lambda acc, f: f(acc), funcs, x)
 
 
 # Tokenize
-def tokenize(text: str) -> Doc:
-    return nlp(text)
+def tokenize(text: str, model=nlp) -> Doc:
+    return model(text)
 
 
 # Batch functions
@@ -65,7 +68,16 @@ def filter_stopwords(batches: list[list[Token]]) -> list[list[Token]]:
     return clean_batches
 
 
-# String operation functions
+def filter_punctuations(batches: list[list[Token]]) -> list[list[Token]]:
+    """Remove stopwords"""
+    clean_batches = []
+    for batch in batches:
+        clean_batch = [token for token in batch if not token.is_punct]
+        clean_batches.append(clean_batch)
+    return clean_batches
+
+
+# Output mode functions
 def to_str(batches: list[list[Token]]) -> list[list[str]]:
     str_batches = []
     for batch in batches:
@@ -80,3 +92,32 @@ def to_str_lower(batches: list[list[Token]]) -> list[list[str]]:
         str_batch = [token.text.lower() for token in batch]
         str_batches.append(str_batch)
     return str_batches
+
+
+def to_lemma(batches: list[list[Token]]) -> list[list[str]]:
+    str_batches = []
+    for batch in batches:
+        str_batch = [token.lemma_ for token in batch]
+        str_batches.append(str_batch)
+    return str_batches
+
+
+def squeeze(multi_dim_list: list) -> list:
+    """Only squeeze the first axis: e.g. (1, 3) -> (3,)"""
+    if len(multi_dim_list) == 1:
+        return multi_dim_list[0]
+    raise Exception(f"挤不出来. First axis size is: {len(multi_dim_list)}")
+
+
+def tf_idf_mean_by_document(texts: list[str], tokenizer: Callable, n_gram: int = 1) -> dict[str, float]:
+    """A wrapper function around TfidfVectorizer class in sklearn"""
+    # Vectorize text
+    tfidf_vectorizer = TfidfVectorizer(tokenizer=tokenizer, ngram_range=(n_gram, n_gram))
+    X = tfidf_vectorizer.fit_transform(texts)
+
+    # Compute each word's average tf_idf value
+    words = tfidf_vectorizer.get_feature_names_out()
+    X_arr = X.toarray()
+    X_mean = np.sum(X_arr, axis=0) / len(X_arr)
+
+    return {word: value for word, value in zip(words, X_mean)}
